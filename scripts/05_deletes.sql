f5378a4b31094615d2f271c9bb60177890beba96
Byannca Camargo <byanncacamargo@gmail.com>
2025-11-29 00:23:07 -0300

    Add SQL script to delete records from tables

---
+++ scripts/05_deletes.sql
@@ -0,0 +1,11 @@
+-- Excluir um cliente
+DELETE FROM Cliente
+WHERE id_cliente = 2;
+
+-- Excluir um horário
+DELETE FROM Horario
+WHERE id_horario = 2;
+
+-- Excluir um serviço
+DELETE FROM Servico
+WHERE id_servico = 2;
